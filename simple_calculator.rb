596c8d39f321ff4e8f3dda4199e9b87dddba392c
Peter Huynh <peterhyh@yahoo.com>
2024-11-30 11:06:59 -0600

    branching logic added

--- simple_calculator.rb
+++ simple_calculator.rb
@@ -17,9 +17,9 @@ def add(first_number, second_number)
   first_number.to_i + second_number.to_i
 end
 
-# def mod(first_number, second_number)
-#   first_number.to_f % second_number.to_f
-# end
+def mod(first_number, second_number)
+  first_number.to_f % second_number.to_f
+end
 
 puts "Simple Calculator"
 20.times {print "-"}
